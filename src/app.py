7b620a3dda2d87a17c91397aeda559f8d319092c
Kacper Celejewski <kacpercele@gmail.com>
2023-03-30 16:37:36 +0200

    part of register page changed

--- src/app.py
+++ src/app.py
@@ -3,9 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/home")
+@app.route("/")
 def Home_Page():
     return render_template("home_page.html")
+
+
+@app.route("/register")
+def register_page():
+    return render_template("register.html")
 
 
 app.run()
