91d3ae70d9e2a46f922e3eebb7b1889c859cd4b2
Kacper Celejewski <kacpercele@gmail.com>
2023-03-30 23:56:32 +0200

    register html updated+class to database created

--- src/app.py
+++ src/app.py
@@ -1,11 +1,31 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+db = SQLAlchemy(app)
 
-@app.route("/")
+
+class User(db.Model):
+    ID = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=40), nulable=False)
+    surrname = db.Column(db.String(length=40), nulable=False, unique=True)
+    email = db.Column(db.String(length=40), nulable=False, unique=True)
+    username = db.Column(db.String(length=40), nulable=False, unique=True)
+    password = db.Column(db.String(length=40), nulable=False, unique=True)
+    ID = db.Column
+
+
+@app.route("/home")
 def Home_Page():
     return render_template("home_page.html")
+
+
+@app.route("/")
+def login_page():
+    return render_template("login_page.html")
 
 
 @app.route("/register")
