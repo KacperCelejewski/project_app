643b0cd71916e0709bb73c9f7ee3cbd05610681c
Kacper Celejewski <kacpercele@gmail.com>
2023-04-08 11:55:35 +0200

    ....

--- src/project/model.py
+++ src/project/model.py
@@ -1,7 +1,6 @@
 from project import db
 
 
-
 class User(db.Model):
     ID = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(length=40), nullable=False)
@@ -10,6 +9,7 @@ class User(db.Model):
     username = db.Column(db.String(length=40), nullable=False, unique=True)
     password = db.Column(db.String(length=60), nullable=False)
    #  projects=db.relationship('Project'),backref='owened_user', lazy=True)
-
+    
     def __repr__(self):
         return f"Name: {self.name}, Surrname: {self.surrname}, Email: {self.email}. Username: {self.username}, Password: {self.password} "
+    
