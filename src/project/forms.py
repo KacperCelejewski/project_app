747107db877691ccd80923b04f286a4a628d1bd5
Wiktor Podwin <272477@student.pwr.edu.pl>
2023-04-08 10:51:34 +0200

    a little update for register modul

--- src/project/forms.py
+++ src/project/forms.py
@@ -1,10 +1,22 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import Length, EqualTo, Email, DataRequired
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+from project.model import User
 
 
 
 class Register_Form(FlaskForm):
+
+    def validate_username(self, username_to_check):
+        user = User.query.filter_by(username=username_to_check.data).first()
+        if user:
+            raise ValidationError("Username already exist! Please try a different username")
+
+    def validate_email(self, email_to_check):    #Klakson zeby dzialal poprawnie modul rejestracji to hasło nie może się składać z samych cyfr bo pokazuje się błąd
+        email = User.query.filter_by(email=email_to_check.data).first()
+        if email:
+            raise ValidationError("Email already exists! Please try a different email")
+
     username= StringField(label="Username: ", validators=[Length(min=2,max=30),DataRequired()])
     surrname= StringField(label="Surrname: ",validators=[Length(min=2,max=30),DataRequired()])
     name= StringField(label="name: ",validators=[Length(min=2,max=30),DataRequired()])
--- src/project/model.py
+++ src/project/model.py
@@ -5,7 +5,7 @@ from project import db
 class User(db.Model):
     ID = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(length=40), nullable=False)
-    surrname = db.Column(db.String(length=40), nullable=False, unique=True)
+    surrname = db.Column(db.String(length=40), nullable=False)
     email = db.Column(db.String(length=40), nullable=False, unique=True)
     username = db.Column(db.String(length=40), nullable=False, unique=True)
     password = db.Column(db.String(length=60), nullable=False)
